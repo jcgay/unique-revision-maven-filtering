00f97caaa528c60175519d38ec2fb380dbb587b5
Jean-Christophe GAY <contact@jeanchristophegay.com>
2018-05-10 11:46:07 +0200

    Add Integration test

---
+++ src/it/replace-revision-in-pom/verify.groovy
@@ -0,0 +1,13 @@
+#!/usr/bin/env groovy
+
+// Check build
+File pomInTarget = new File("$basedir/target/pom.xml")
+def projectInTarget = new XmlSlurper().parse(pomInTarget)
+assert projectInTarget.version == '1.0-SNAPSHOT'
+
+// Check local repository
+File pom = new File("$localRepositoryPath/fr/jcgay/maven/extension/urmf/replace-revision-in-pom/1.0-SNAPSHOT/replace-revision-in-pom-1.0-SNAPSHOT.pom")
+def project = new XmlSlurper().parse(pom)
+assert project.version == '1.0-SNAPSHOT'
+
+return true
